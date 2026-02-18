b7e09d976bf28224a94a2b5b4a57ee70bad89e9c
ramyasree-max <rdondlapadu@gmail.com>
2026-02-18 22:41:52 +0530

    Refactor variable names and improve formatting

--- ps1b.py
+++ ps1b.py
@@ -1,25 +1,26 @@
 
 # Part B - Saving with a Raise
-#inputs
-annual_salary = float(input("Enter your annual salary: "))
-portion_saved = float(input("Enter the percent of your salary to save : "))
-total_cost = float(input("Enter the cost of your dream home: "))
-semi_annual_raise = float(input("Enter the semi-annual raise : "))
+#inputs required
+AnnualSalary = float(input("Enter your annual salary: "))
+PortionSaved = float(input("Enter the percent of your salary to save : "))
+TotalCost = float(input("Enter the cost of your dream home: "))
+SemiAnnualRaise = float(input("Enter the semi-annual raise : "))
 
-portion_down_payment = 0.25
-down_payment = total_cost * portion_down_payment
+PortionDownPayment = 0.25
+DownPayment = TotalCost * PortionDownPayment
 
-current_savings = 0
+CurrenSavings = 0
 r = 0.04
 months = 0
 
-while current_savings < down_payment:
-    monthly_salary = annual_salary / 12
-    current_savings += current_savings * (r / 12)
-    current_savings += monthly_salary * portion_saved
+while CurrentSavings < DownPayment:
+    MonthlySalary = AnnualSalary / 12
+    CurrentSavings += CurrentSavings * (r / 12)
+    CurrentSavings += MonthlySalary * PortionSaved
     months += 1
 
     if months % 6 == 0:
-        annual_salary += annual_salary * semi_annual_raise
-#output
+        AnnualSalary += AnnualSalary * SemiAnnualRaise
+# required output
 print("Number of months:", months)
+
