3711e5364056ebf607d4a06c2bdd673894f5d88e
ramyasree-max <rdondlapadu@gmail.com>
2026-02-18 22:09:30 +0530

    Refactor input variables and loop logic
    Refactor variable names to use camel case and update comments.

--- ps1a.py
+++ ps1a.py
@@ -1,23 +1,23 @@
-#Inputs as per we required
-annual_salary=int(input("Enter your annual salary : "))
-portion_saved=float(input("Enter monthly saving rate : "))
-total_cost=int(input("Enter total cost of dream house : "))
+#Inputs required
+AnnualSalary=int(input("Enter your annual salary : "))
+PortionSaved=float(input("Enter monthly saving rate : "))
+TotalCost=int(input("Enter total cost of dream house : "))
 
-#Constants given in assignment
-postion_of_down_payment=0.25
-annual_return=0.04
+PostionOfDownPayment=0.25
+AnnualReturn=0.04
 
 #Initial values
-current_savings=0
-monthly_salary=annual_salary/12
-down_payment=total_cost*postion_of_down_payment
+CurrentSavings=0
+MonthlySalary=AnnualSalary/12
+DownPayment=TotalCost*PostionOfDownPayment
 months=0
 
-#Loops
-while current_savings<down_payment:
-      current_savings+=current_savings*(annual_return/12)
-      current_savings+=monthly_salary*portion_saved
+#By using while loop
+while CurrentSavings<down_payment:
+      CurrentSavings+=CurrentSavings*(annual_return/12)
+      CurrentSavings+=MonthlySalary*PortionSaved
       months+=1
       
-#Output to show the no.of months
+#No.of months required
+
 print(f"Number of months : {months}")
